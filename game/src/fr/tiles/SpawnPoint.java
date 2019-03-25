832b2c7a93a65cca6a1164ef806d49f1c24fe5a3
ABELHAJ <renardtheyou@gmail.com>
2019-03-25 15:33:26 +0100

    comment SpawnPoint

--- game/src/fr/tiles/SpawnPoint.java
+++ game/src/fr/tiles/SpawnPoint.java
@@ -4,7 +4,9 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 
 import fr.application.Application;
-
+/**
+ * Zone d'apparition
+ */
 public class SpawnPoint extends MapTileSuper {
 
 	private static final long serialVersionUID = 1L;
@@ -13,28 +15,42 @@ public class SpawnPoint extends MapTileSuper {
 	private static final boolean destroyable = false;
 	private static final boolean walkable = true;
 
-
+	/**
+	 * constructeur SpawnPoint
+	 * @param x : abscisse du point ou est placé l'element
+	 * @param y : ordonnée du point ou est placé l'element
+	 */
 	public SpawnPoint(int x, int y) {
 		super(x, y);
 	}
-
+	/**
+	 * affiche la zone d'apparition
+	 * @param g : permet l'affichage
+	 */
 	@Override
 	public void draw(Graphics2D g) {
 		g.setColor(c);
 		g.fillRect(pos.getIX(), pos.getIY(), SIZE, SIZE);
 	}
-
+	/**
+	 * permet d'interagir avec la zone d'apparition
+	 * @param m : l'application
+	 */
 	@Override
 	public void interact(Application m) {
 		// TODO Auto-generated method stub
 
 	}
-
+	/**
+	 * @return destroyable : true si l'element est destructible et false sinon
+	 */
 	@Override
 	public boolean isDestroyable() {
 		return destroyable;
 	}
-
+	/**
+	 * @return walkable : true si on peut marcher sur l'element et false sinon
+	 */
 	@Override
 	public boolean isWalkable() {
 		return walkable;
