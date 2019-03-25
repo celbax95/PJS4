5d9350b550ddfb66ade704245686dc1a16db2f3f
ABELHAJ <renardtheyou@gmail.com>
2019-03-25 15:24:07 +0100

    comment Floor

--- game/src/fr/tiles/Floor.java
+++ game/src/fr/tiles/Floor.java
@@ -6,7 +6,9 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 
 import fr.application.Application;
-
+/**
+ * Sol
+ */
 public class Floor extends MapTileSuper {
 
 	private static final long serialVersionUID = 1L;
@@ -16,27 +18,41 @@ public class Floor extends MapTileSuper {
 	private static final boolean walkable = true;
 
 	private static final boolean destroyable = false;
-
+	/**
+	 * constructeur Floor
+	 * @param x : abscisse du point ou est placé le sol
+	 * @param y : ordonnée du point ou est placé le sol
+	 */
 	public Floor(int x, int y) {
 		super(x, y);
 	}
-
+	/**
+	 * affiche le sol
+	 * @param g : permet l'affichage
+	 */
 	@Override
 	public void draw(Graphics2D g) {
 		g.drawImage(img, pos.getIX(), pos.getIY(), null);
 	}
-
+	/**
+	 * permet d'interagir avec un element
+	 * @param m : l'application
+	 */
 	@Override
 	public void interact(Application m) {
 		// TODO Auto-generated method stub
 
 	}
-
+	/**
+	 * @return destroyable : true si l'element est destructible et false sinon
+	 */
 	@Override
 	public boolean isDestroyable() {
 		return destroyable;
 	}
-
+	/**
+	 * @return walkable : true si on peut marcher sur l'element et false sinon
+	 */
 	@Override
 	public boolean isWalkable() {
 		return walkable;
