046b211dd6d34127999cbb921a0697a287531801
celbax95 <loicmace95@gmail.com>
2019-03-25 13:29:20 +0100

    commentaires ajoutes

--- game/src/fr/itemsApp/bomb/BombFactory.java
+++ game/src/fr/itemsApp/bomb/BombFactory.java
@@ -16,7 +16,7 @@ public class BombFactory implements Serializable {
 
 	/**
 	 * Creation d'un bombe
-	 * 
+	 *
 	 * @param type        : type de la bombe a creer
 	 * @param application : Application
 	 * @param creator     : Character qui a place la bombe
@@ -26,9 +26,11 @@ public class BombFactory implements Serializable {
 
 		GameMap map = application.getMap();
 
+		// Recuperation de la tile sur laquelle le joueur est
 		Point tile = creator.getCenter();
 		tile = map.getTileFor(tile.x, tile.y);
 
+		// Creation de la bombe
 		switch (type.trim().toLowerCase()) {
 		case "std":
 			return new BombStd(tile, map.getTileSize(), 1000, 1);
