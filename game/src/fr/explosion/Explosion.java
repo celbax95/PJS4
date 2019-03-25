dc633f1ca172bf74be69eed85884034794e48c10
ABELHAJ <renardtheyou@gmail.com>
2019-03-25 13:12:46 +0100

    comment Explosion

--- game/src/fr/explosion/Explosion.java
+++ game/src/fr/explosion/Explosion.java
@@ -3,7 +3,11 @@ package fr.explosion;
 import fr.scale.Scale;
 import fr.util.point.Point;
 import fr.util.time.Cooldown;
-
+/**
+ * 
+ * Classe abstraite Explosion
+ *
+ */
 public abstract class Explosion implements IExplosion {
 
 	private static final long serialVersionUID = 1L;
@@ -19,7 +23,10 @@ public abstract class Explosion implements IExplosion {
 	protected int tileSize;
 
 	protected Cooldown explosionTime;
-
+	
+	/**
+	 * constructeur Explosion
+	 */
 	public Explosion() {
 		tileSize = TILE_SIZE;
 		type = 0;
@@ -28,27 +35,46 @@ public abstract class Explosion implements IExplosion {
 		this.damage = 0;
 		this.explosionTime = new Cooldown(0);
 	}
-
+	/**
+	 * 
+	 * @param tile : élément du jeu possedant une largeur et une hauteur 
+	 * @return la position de la tile
+	 */
 	private Point getPosfromTile(Point tile) {
 		return new Point(tile.x * tileSize, tile.y * tileSize);
 	}
-
+	
+	/**
+	 * @param explosionTime : temps mis par l'explosion avant de disparaitre du terrain
+	 */
 	@Override
-	public void setCooldown(int coolDown) {
-		this.explosionTime = new Cooldown(coolDown);
+	public void setCooldown(int explosionTime) {
+		this.explosionTime = new Cooldown(explosionTime);
 	}
-
+	
+	/**
+	 * 
+	 * @param damage : dommage provoqués par une explosion
+	 */
 	@Override
 	public void setDamage(double damage) {
 		this.damage = damage;
 	}
-
+	
+	/**
+	 * 
+	 * @param tile : élément du jeu possedant une largeur et une hauteur 
+	 */
 	@Override
 	public void setTile(Point tile) {
 		this.tile = tile;
 		this.pos = getPosfromTile(tile);
 	}
-
+	
+	/**
+	 * 
+	 * @param type : type de l'explosion
+	 */
 	@Override
 	public void setType(int type) {
 		this.type = type;
