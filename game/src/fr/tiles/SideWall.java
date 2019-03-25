d7fbb5d57cb469855a8eb2a0e69b90abef8c4fb4
ABELHAJ <renardtheyou@gmail.com>
2019-03-25 15:27:07 +0100

    comment SideWall

--- game/src/fr/tiles/SideWall.java
+++ game/src/fr/tiles/SideWall.java
@@ -4,7 +4,9 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 
 import fr.application.Application;
-
+/**
+ * Mur
+ */
 public class SideWall extends MapTileSuper {
 
 	private static final long serialVersionUID = 1L;
@@ -13,28 +15,43 @@ public class SideWall extends MapTileSuper {
 	private static final boolean walkable = false;
 
 	private static final boolean destroyable = false;
-
+	/**
+	 * Constructeur SideWall
+	 * @param x : abscisse du point ou est placé l'element
+	 * @param y : ordonnée du point ou est placé l'element
+	 */
 	public SideWall(int x, int y) {
 		super(x, y);
 	}
-
+	/**
+	 * affiche le mur
+	 * @param g : permet l'affichage
+	 */
 	@Override
 	public void draw(Graphics2D g) {
 		g.setColor(c);
 		g.fillRect(pos.getIX(), pos.getIY(), SIZE, SIZE);
 	}
-
+	
+	/**
+	 * permet d'interagir avec le mur
+	 * @param m : l'application
+	 */
 	@Override
 	public void interact(Application m) {
 		// TODO Auto-generated method stub
 
 	}
-
+	/**
+	 * @return destroyable : true si l'element est destructible et false sinon
+	 */
 	@Override
 	public boolean isDestroyable() {
 		return destroyable;
 	}
-
+	/**
+	 * @return walkable : true si on peut marcher sur l'element et false sinon
+	 */
 	@Override
 	public boolean isWalkable() {
 		return walkable;
