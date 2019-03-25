7db1c3ecc66401fbeb2a868aeba4fa528d82cf0b
celbax95 <loicmace95@gmail.com>
2019-03-25 13:01:40 +0100

    javadoc ajoutee

--- game/src/fr/itemsApp/bomb/IBomb.java
+++ game/src/fr/itemsApp/bomb/IBomb.java
@@ -8,21 +8,41 @@ import fr.explosion.IExplosion;
 import fr.itemsApp.Manageable;
 import fr.util.point.Point;
 
+/**
+ * Interface pour une bombe
+ */
 public interface IBomb extends Serializable, Manageable, fr.itemsApp.Drawable {
 
 	@Override
 	void draw(Graphics2D g);
 
-	void explode(Application a);
+	/**
+	 * Fait exploser la bombe @see Explosion
+	 *
+	 * @param application : application
+	 */
+	void explode(Application application);
 
+	/**
+	 * @return Type de l'explosion a creer
+	 */
 	IExplosion getExplosion();
 
+	/**
+	 * @return Taille de l'explosion de la bombe
+	 */
 	int getExplosionSize();
 
+	/**
+	 * @return Emplacement de la bombe sur la map
+	 */
 	Point getTile();
 
 	@Override
 	void manage(Application a, double t);
 
+	/**
+	 * Lancement du cooldown de la bombe
+	 */
 	void start();
 }
