e0ca42b6ce03b16089e290f938efa1bf9ded20cd
celbax95 <loicmace95@gmail.com>
2019-03-25 17:03:22 +0100

    javadoc ajoute

--- game/src/fr/screen/keyboard/KeyBoard.java
+++ game/src/fr/screen/keyboard/KeyBoard.java
@@ -5,17 +5,26 @@ import java.awt.event.KeyListener;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Gestion du clavier
+ */
 public class KeyBoard implements KeyListener {
 
 	private static List<Integer> keys = new LinkedList<Integer>();
 	private static boolean released = true;
 
+	/**
+	 * Ajout de la touche pressee dans la liste de touches pressees
+	 */
 	@Override
 	public void keyPressed(KeyEvent e) {
 		if (!keys.contains(e.getKeyCode()))
 			keys.add(e.getKeyCode());
 	}
 
+	/**
+	 * Supression de la touche pressee dans la liste de touches pressees
+	 */
 	@Override
 	public void keyReleased(KeyEvent e) {
 		keys.remove((Object) e.getKeyCode());
@@ -25,24 +34,43 @@ public class KeyBoard implements KeyListener {
 	public void keyTyped(KeyEvent e) {
 	}
 
+	/**
+	 * @param i : indice de la touche a recuperer dans la liste de touches pressees
+	 * @return : la touche a l'indice i
+	 */
 	public static int getKey(int i) {
 		return keys.get(i);
 	}
 
+	/**
+	 * @return le liste de toutes les touches pressees
+	 */
 	public static List<Integer> getKeys() {
 		return new LinkedList<>(keys);
 	}
 
+	/**
+	 * @return une touche est pressee
+	 */
 	public static boolean isPressed() {
 		released = false;
 		return !(keys.isEmpty());
 	}
 
+	/**
+	 * @param key : un touche
+	 * @return la touche key est pressee
+	 */
 	public static boolean isPressed(int key) {
 		released = false;
 		return keys.contains(key);
 	}
 
+	/**
+	 * Tapotage du clavier
+	 * 
+	 * @return la clavier a ete tapote
+	 */
 	public static boolean tap() {
 		if (!(keys.isEmpty()) && released)
 			return true;
