1b859f3399346caa6b02fb768f5e6015d19b3c87
ABELHAJ <renardtheyou@gmail.com>
2019-03-25 15:10:51 +0100

    comment FactoryTile

--- game/src/fr/tiles/FactoryTile.java
+++ game/src/fr/tiles/FactoryTile.java
@@ -2,11 +2,20 @@ package fr.tiles;
 
 import fr.map.IFactoryTile;
 import fr.map.MapTile;
-
+/**
+ * Fabrique d'elements de terrain
+ */
 public class FactoryTile implements IFactoryTile {
 
 	private static final long serialVersionUID = 1L;
-
+	
+	/**
+	 * Creer un element de terrain
+	 * @param type : type de l'element
+	 * @param x : abscisse du point ou est placé l'element
+	 * @param y : ordonnée du point ou est placé l'element
+	 * @return l'element de terrain
+	 */
 	@Override
 	public MapTile create(char type, int x, int y) {
 		switch (type) {
