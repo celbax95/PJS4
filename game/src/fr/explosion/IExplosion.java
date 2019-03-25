310e0ed664b2e1ca49e10e8ac3c76832c7b8054e
ABELHAJ <renardtheyou@gmail.com>
2019-03-25 12:53:44 +0100

    comment interface Explosion

--- game/src/fr/explosion/IExplosion.java
+++ game/src/fr/explosion/IExplosion.java
@@ -5,13 +5,29 @@ import java.io.Serializable;
 import fr.itemsApp.Drawable;
 import fr.itemsApp.Manageable;
 import fr.util.point.Point;
-
+/**
+ * Interface Explosion, permet de créer un comportement generique pour les explosions
+ */
 public interface IExplosion extends Drawable, Manageable, Serializable {
-	public void setCooldown(int cd);
-
+	/**
+	 * 
+	 * @param explosionTime : temps mis par l'explosion avant de disparaitre du terrain
+	 */
+	public void setCooldown(int explosionTime);
+	
+	/**
+	 * 
+	 * @param damage : dommage provoqués par une explosion
+	 */
 	public void setDamage(double damage);
-
+	/**
+	 * 
+	 * @param tile : élément du jeu possedant une largeur et une hauteur 
+	 */
 	public void setTile(Point tile);
-
+	/**
+	 * 
+	 * @param type : type de l'explosion
+	 */
 	public void setType(int type);
 }
