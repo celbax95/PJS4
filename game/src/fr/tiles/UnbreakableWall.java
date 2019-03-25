720e9152e41e1e6f6c4f0c81dffc3e9d5840f934
ABELHAJ <renardtheyou@gmail.com>
2019-03-25 15:38:00 +0100

    comment UnbreakableWall

--- game/src/fr/tiles/UnbreakableWall.java
+++ game/src/fr/tiles/UnbreakableWall.java
@@ -4,7 +4,9 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 
 import fr.application.Application;
-
+/**
+ * Mur Incassable
+ */
 public class UnbreakableWall extends MapTileSuper {
 
 	private static final long serialVersionUID = 1L;
@@ -12,28 +14,41 @@ public class UnbreakableWall extends MapTileSuper {
 
 	private static final boolean walkable = false;
 	private static final boolean destroyable = false;
-
+	/**
+	 * Constructeur UnbreakableWall
+	 * @param x : abscisse du point ou est placé l'element
+	 * @param y : ordonnée du point ou est placé l'element
+	 */
 	public UnbreakableWall(int x, int y) {
 		super(x, y);
 	}
-
+	/**
+	 * affiche le mur incassable
+	 * @param g : permet l'affichage
+	 */
 	@Override
 	public void draw(Graphics2D g) {
 		g.setColor(c);
 		g.fillRect(pos.getIX(), pos.getIY(), SIZE, SIZE);
 	}
-
+	/**
+	 * permet d'interagir avec le mur incassable
+	 * @param m : l'application
+	 */
 	@Override
 	public void interact(Application m) {
 		// TODO Auto-generated method stub
-
 	}
-
+	/**
+	 * @return destroyable : true si l'element est destructible et false sinon
+	 */
 	@Override
 	public boolean isDestroyable() {
 		return destroyable;
 	}
-
+	/**
+	 * @return walkable : true si on peut marcher sur l'element et false sinon
+	 */
 	@Override
 	public boolean isWalkable() {
 		return walkable;
