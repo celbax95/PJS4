dab3bbeb120548228035fc7de3a48256f18137e0
celbax95 <loicmace95@gmail.com>
2019-03-25 11:54:14 +0100

    javaDoc ajoute

--- game/src/fr/screen/AppliScreen.java
+++ game/src/fr/screen/AppliScreen.java
@@ -3,15 +3,40 @@ package fr.screen;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
+/**
+ * Application
+ *
+ * @see MainJPanel
+ */
 public interface AppliScreen {
 
+	/**
+	 * Ferme l'application
+	 */
 	void close();
 
+	/**
+	 * Appele par MainJPanel
+	 *
+	 * @see MainJpanel
+	 *
+	 * @param g : Permet d'afficher a l'ecran
+	 * @throws EndApp : Fin d'application
+	 */
 	void draw(Graphics2D g) throws EndApp;
 
+	/**
+	 * @return Couleur d'arriere plan de l'application
+	 */
 	Color getBackgroundColor();
 
+	/**
+	 * @return Nom de la fenetre
+	 */
 	String getName();
 
+	/**
+	 * Lance le thread
+	 */
 	void start();
 }
