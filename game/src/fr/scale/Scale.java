cadead068ebc591c7c8fa3e817dba9c4137eda49
ABELHAJ <renardtheyou@gmail.com>
2019-03-25 16:11:06 +0100

    comment Scale

--- game/src/fr/scale/Scale.java
+++ game/src/fr/scale/Scale.java
@@ -1,16 +1,26 @@
 package fr.scale;
-
+/**
+ * Echelle, permet de redimentionner l'affichage (fonctionnalite de zoom)
+ * Utilise un ratio par lequel on multiplie la taille des elements a afficher
+ */
 public class Scale {
 
 	private static double scale = 1;
-
+	/**
+	 * constructeur Scale vide
+	 */
 	private Scale() {
 	}
-
+	/**
+	 * @return l'echelle
+	 */
 	public static double getScale() {
 		return scale;
 	}
-
+	/**
+	 * 
+	 * @param s : l'echelle
+	 */
 	public static void setScale(double s) {
 		scale = s;
 	}
