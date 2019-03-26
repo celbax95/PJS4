522a007c01c6834d23548bc84459a01a1ce4172b
ABELHAJ <renardtheyou@gmail.com>
2019-03-26 21:31:24 +0100

    HUD enhanced rendering

--- game/src/fr/hud/HUD.java
+++ game/src/fr/hud/HUD.java
@@ -20,12 +20,24 @@ import fr.itemsApp.character.ICharacter;
  */
 public class HUD implements Drawable, Serializable, Manageable{
 	private static final long serialVersionUID = 1L;
-	//private static int SIZE = (int) (DEFAULT_SIZE * Scale.getScale());
 	
-	private int lifeBarAmount, lifeBarMaxAmount;
-	private static final int WIDTH = 200, HEIGHT = 50;
-	private static Image image = (new ImageIcon(HUD.class.getResource("/images/HUD/hud.png")))
+	private static final int WIDTH = 510, HEIGHT = 230;
+	
+	private static final int RECTANGLE_HEIGHT = 26, RECTANGLE_MAX_WIDTH = 200;
+	
+	private static final int LIFE_MAX_AMOUNT = 100;
+	
+	private int currentLifeAmount, currentCoolDownAmount;
+	
+	private float lifeRectangleWidth = RECTANGLE_MAX_WIDTH, coolDownRegtangleWidth = RECTANGLE_MAX_WIDTH;
+	
+	private static Image hudGif = (new ImageIcon(HUD.class.getResource("/images/HUD/hud.gif")))
 	.getImage().getScaledInstance(WIDTH, HEIGHT, Image.SCALE_DEFAULT);
+	
+	private static Image hudPng = (new ImageIcon(HUD.class.getResource("/images/HUD/hud.png")))
+			.getImage().getScaledInstance(WIDTH, HEIGHT, Image.SCALE_DEFAULT);
+	
+	
 	private Font font = new Font("Arial", Font.PLAIN, 24);
 	
 	/**
@@ -33,18 +45,24 @@ public class HUD implements Drawable, Serializable, Manageable{
 	 * @param player : Le joueur 
 	 */
 	public HUD(ICharacter player) {
-		lifeBarAmount = player.getHealth();
-		lifeBarMaxAmount = 100;
+		currentLifeAmount = player.getHealth();
 	}
 	/**
 	 * Permet d'afficher les elements de l'HUD
 	 * @param g : Permet l'affichage
 	 */
 	public void draw(Graphics2D g) {
-		g.drawImage(image, 0, 20, null);
-		g.setFont(font);
-		g.setColor(Color.WHITE);
-		g.drawString(lifeBarAmount + "/" + lifeBarMaxAmount, 70, 55);
+		g.drawImage(hudPng, 0, 0, null);
+		g.drawImage(hudGif, 0, 0, null);
+		g.setColor(Color.BLACK);
+		// test
+		currentLifeAmount = 30;
+		if(currentLifeAmount < LIFE_MAX_AMOUNT)
+			lifeRectangleWidth = ((float)((LIFE_MAX_AMOUNT - currentLifeAmount) / 100.0) * RECTANGLE_MAX_WIDTH);
+		coolDownRegtangleWidth = 1;
+        g.fillRect(222 + (RECTANGLE_MAX_WIDTH - ((LIFE_MAX_AMOUNT - currentLifeAmount)*2)),
+        		22,(int)lifeRectangleWidth, RECTANGLE_HEIGHT);
+        g.fillRect(222, 76,(int)coolDownRegtangleWidth, RECTANGLE_HEIGHT);
 	}
 	/**
 	 * Gestion de l'element
