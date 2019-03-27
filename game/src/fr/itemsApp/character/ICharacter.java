f721e2a780f460e24a8a208b71d19fe47f6356e0
ABELHAJ <renardtheyou@gmail.com>
2019-03-27 11:57:57 +0100

    add health

--- game/src/fr/itemsApp/character/ICharacter.java
+++ game/src/fr/itemsApp/character/ICharacter.java
@@ -13,8 +13,10 @@ public interface ICharacter extends Drawable, Serializable, Manageable {
 	/**
 	 * Execute les action suivant les touches du clavier
 	 *
-	 * @param application    : Application
-	 * @param clickedKeys : Touches appuyees du clavier
+	 * @param application
+	 *            : Application
+	 * @param clickedKeys
+	 *            : Touches appuyees du clavier
 	 */
 	void actions(Application application, List<Integer> clickedKeys);
 
@@ -23,24 +25,30 @@ public interface ICharacter extends Drawable, Serializable, Manageable {
 	 */
 	Point getCenter();
 
+	int getHealth();
+
 	/**
 	 * Change le cooldown de pose des bombes
-	 * 
-	 * @param bombCoolDown : nouveau cooldown
+	 *
+	 * @param bombCoolDown
+	 *            : nouveau cooldown
 	 */
 	void setBombCoolDown(int bombCoolDown);
 
+	void setHealth(int health);
+
 	/**
 	 * Change la position du ICharacter
-	 * 
+	 *
 	 * @param pos
 	 */
 	void setPos(Point pos);
 
 	/**
 	 * Change la vitesse du ICharacter
-	 * 
-	 * @param speed : nouvelle vitesse pour le ICharacter
+	 *
+	 * @param speed
+	 *            : nouvelle vitesse pour le ICharacter
 	 */
 	void setSpeed(int speed);
 }
